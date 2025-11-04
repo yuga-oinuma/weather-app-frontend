5185e9974f5a0066f21aa3e7da8198581e0327cf
yuga-oinuma <oinuma@augusta-lab.co.jp>
2025-11-04 16:26:30 +0900

    fix: 表示する都市を修正

--- app/src/constants/location.ts
+++ app/src/constants/location.ts
@@ -3,21 +3,21 @@ export const CITY_NAME_MAP: Record<string, string> = {
   Sendai: "仙台",
   Tokyo: "東京",
   Nagoya: "名古屋",
-  Kanazawa: "金沢",
-  Kochi: "高知",
-  Fukuoka: "福岡",
+  //  Kanazawa: "金沢",
+  // Kochi: "高知",
+  // Fukuoka: "福岡",
   Naha: "那覇",
 };
 
 export const CITY_COLOR_MAP: { [key: string]: string } = {
-  Sapporo: "#64b5f6",   // 水色（北海道の雪・氷をイメージ）
-  Sendai: "#81c784",    // 緑（杜の都をイメージ）
-  Tokyo: "#e53935",     // 赤（首都・活気をイメージ）
-  Nagoya: "#ff9800",    // オレンジ（中部地方の温暖さをイメージ）
-  Kanazawa: "#fbc02d",  // 金色（金沢の金箔をイメージ）
-  Kochi: "#66bb6a",     // 深緑（四国の自然をイメージ）
-  Fukuoka: "#ab47bc",   // 紫（九州の花をイメージ）
-  Naha: "#26c6da",      // 青緑（沖縄の海をイメージ）
+  Sapporo: "#64b5f6",
+  Sendai: "#81c784",  
+  Tokyo: "#e53935",   
+  Nagoya: "#ff9800",
+  // Kanazawa: "#fbc02d",
+  // Kochi: "#66bb6a",
+  // Fukuoka: "#ab47bc",
+  Naha: "#26c6da",
 };
 
 export const CITIES = [
@@ -25,8 +25,8 @@ export const CITIES = [
   "Sendai", 
   "Tokyo", 
   "Nagoya", 
-  "Kanazawa", 
-  "Kochi", 
-  "Fukuoka", 
+  // "Kanazawa",
+  // "Kochi",
+  // "Fukuoka",
   "Naha"
 ];
